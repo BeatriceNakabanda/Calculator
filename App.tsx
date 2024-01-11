b15ee139dc81232e0d160f744e75e0878f18641b
Beats <nakabandabeatrice@gmail.com>
2024-01-11 16:56:22 +0300

    Updated UI

--- App.tsx
+++ App.tsx
@@ -29,31 +29,32 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+
+// function Section({children, title}: SectionProps): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,41 +63,48 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+    // <SafeAreaView style={backgroundStyle}>
+    //    <StatusBar
+    //     barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+    //     backgroundColor={backgroundStyle.backgroundColor}
+    //   /> 
+    //   <ScrollView
+    //     contentInsetAdjustmentBehavior="automatic"
+    //     style={backgroundStyle}>
+    //  <Header /> 
         <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          style={styles.container}>
+            <View style={styles.half1}>
+
+            </View>
+            <View style={styles.half2}>
+
+            </View>
+            <View style={styles.half3}>
+
+            </View>
+            <View style={styles.half4}>
+
+            </View>
+          
         </View>
-      </ScrollView>
-    </SafeAreaView>
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color:'blue'  
+},
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -112,7 +120,32 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  }, 
+  half1: {
+    flex: 1,
+    backgroundColor: 'white'
   },
+  half2: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  half21: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+  half22: {
+    flex: 1,
+    backgroundColor: 'blue'
+  },
+  half3: {
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  half4: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+
 });
 
 export default App;
